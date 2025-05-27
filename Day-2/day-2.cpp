7e40d7e26b93e1f80d978c425fb08a6748d7f834
vivek <vivekroy2584@gmail.com>
2025-05-27 05:42:17 +0000

    day-2-part-2

--- Day-2/day-2.cpp
+++ Day-2/day-2.cpp
@@ -7,8 +7,46 @@
 #include <cmath>
 #include <algorithm>
 
+bool is_safe(const std::vector<int>& levels){
+    bool increasing{true};
+    bool decreasing{true};
+
+    for(size_t i = 1; i < levels.size(); i++){
+        if(levels[i] > levels[i-1]){
+                decreasing = false;
+        }
+        else if(levels[i] < levels[i-1]){
+                increasing = false;
+        }
+        else{
+            decreasing = false;
+            increasing = false;
+        }
+    } 
+
+    bool gradual{true};
+    for(size_t i = 1; i < levels.size(); i++){
+        if(std::abs(levels[i] - levels[i-1]) < 1 || std::abs(levels[i] - levels[i-1]) > 3){
+                gradual = false;
+        }
+    }
+
+    if((increasing || decreasing) && gradual) return true;
+    else return false;
+}
+
+bool has_one_unsafe_level(const std::vector<int>& levels){
+    for(size_t i = 0; i < levels.size(); i++){
+        std::vector<int> new_levels;
+        for(size_t j = 0; j < levels.size(); j++){
+            if(j != i) new_levels.push_back(levels[j]);
+        }
+        if(is_safe(new_levels)) return true;
+    }
+    return false;
+}
+
 int main(int argc, char** argv){
-    // part-1 START
     std::string input_file = argv[1];
     std::ifstream file(input_file);
     std::string line;
@@ -24,30 +62,14 @@ int main(int argc, char** argv){
             levels.push_back(temp);
         }
 
-        bool increasing{true};
-        bool decreasing{true};
-
-        for(size_t i = 1; i < levels.size(); i++){
-            if(levels[i] > levels[i-1]){
-                decreasing = false;
-            }
-            else if(levels[i] < levels[i-1]){
-                increasing = false;
-            }
-            else{
-                decreasing = false;
-                increasing = false;
-            }
-        } 
-
-        bool gradual{true};
-        for(size_t i = 1; i < levels.size(); i++){
-            if(std::abs(levels[i] - levels[i-1]) < 1 || std::abs(levels[i] - levels[i-1]) > 3){
-                gradual = false;
+        if(is_safe(levels)){
+            count++;
+        }
+        else{
+            if(has_one_unsafe_level(levels)){
+                count++;
             }
         }
-
-        if((increasing || decreasing) && gradual) count++;
     }
 
     std::cout << count << "\n";
